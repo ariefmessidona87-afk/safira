54917f043474bc8a3f3fe1af72c35459736b3b0d
ariefmessidona87-afk <ariefmessidona87@gmail.com>
2025-10-16 11:53:35 +0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,21 @@
+body {
+    background-color: #FFFFE0; /* Warna kuning cerah */
+    font-family: Arial, sans-serif; /* jenis huruf yang bersih dan mudah 
+    color: #191970; /* Warna teks utama (biru gelap) */
+}
+
+.kartu profil {
+    widht:300px;
+    margin: 50px auto; /* 50px jarak atas/bawah,auto menengahkan kiri
+    padding: 20px;
+    background-color: white;
+    border-radius: 15px; /* Membuat sudut menjadi melengkung */
+    box-shadow: 0 4px 8px rgba(0,0,0,0.1); /* efek bayangan */
+    text-align: center; /*
+}
+
+
+.kartu profil img {
+    Width: 150px;
+    border-radius: 50%; /* trik untuk memuat gambar persegi menjadi lingkaran sempurna 
+}
